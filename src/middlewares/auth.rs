5ea2e30e22f37291418f958868ba3526c3ac57e6
dillontl <dillon@lueraws.com>
2024-02-19 13:52:18 -0700

    works but doesnt authenticate

---
+++ src/middlewares/auth.rs
@@ -0,0 +1,90 @@
+use axum::{
+    async_trait,
+    extract::{FromRef, FromRequestParts},
+    http::{self, request::Parts, StatusCode},
+    response::{IntoResponse, Response},
+};
+use firebase_auth::{FirebaseAuthState, FirebaseProvider};
+use serde::{Deserialize, Serialize};
+use tracing::debug;
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct FirebaseUser {
+    pub iss: String,
+    pub aud: String,
+    pub sub: String,
+    pub iat: u64,
+    pub exp: u64,
+    pub auth_time: u64,
+    pub user_id: String,
+    pub provider_id: Option<String>,
+    pub name: Option<String>,
+    pub picture: Option<String>,
+    pub email: Option<String>,
+    pub email_verified: Option<bool>,
+    pub firebase: FirebaseProvider,
+
+    #[serde(rename = "https://hasura.io/jwt/claims")]
+    pub hasura: HasuraClaims,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct HasuraClaims {
+    pub x_hasura_default_role: String,
+    pub x_hasura_allowed_roles: Vec<String>,
+    pub x_hasura_user_id: String,
+}
+
+fn get_bearer_token(header: &str) -> Option<String> {
+    let prefix_len = "Bearer ".len();
+
+    match header.len() {
+        l if l < prefix_len => None,
+        _ => Some(header[prefix_len..].to_string()),
+    }
+}
+
+#[async_trait]
+impl<S> FromRequestParts<S> for FirebaseUser
+    where
+        FirebaseAuthState: FromRef<S>,
+        S: Send + Sync,
+{
+    type Rejection = UnauthorizedResponse;
+
+    async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let store = FirebaseAuthState::from_ref(state);
+
+        let auth_header = parts
+            .headers
+            .get(http::header::AUTHORIZATION)
+            .and_then(|value| value.to_str().ok())
+            .unwrap_or("");
+
+        let bearer = get_bearer_token(auth_header).map_or(
+            Err(UnauthorizedResponse {
+                msg: "Unauthorized Request".to_string(),
+            }),
+            Ok,
+        )?;
+
+        debug!("Got bearer token {}", bearer);
+
+        match store.firebase_auth.verify(&bearer) {
+            Err(e) => Err(UnauthorizedResponse {
+                msg: format!("Failed to verify Token: {}", e),
+            }),
+            Ok(current_user) => Ok(current_user),
+        }
+    }
+}
+
+pub struct UnauthorizedResponse {
+    msg: String,
+}
+
+impl IntoResponse for UnauthorizedResponse {
+    fn into_response(self) -> Response {
+        (StatusCode::UNAUTHORIZED, format!("{}", self.msg)).into_response()
+    }
+}
